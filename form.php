497834f46e9624a2170a44fa263c4a3988668406
Teja Sudeep <44145755+codeteja@users.noreply.github.com>
2018-10-15 03:07:10 +0530

    form.php

---
+++ form.php
@@ -0,0 +1,15 @@
+<form action="insert.php" method="post" enctype="multipart/form-data">
+    <p>
+        <label for="firstName">First Name:</label>
+        <input type="text" name="first_name" id="firstName">
+    </p>
+    <p>
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="last_name" id="lastName">
+    </p>
+    <p>
+        <label for="emailAddress">Email Address:</label>
+        <input type="text" name="email" id="emailAddress">
+    </p>
+    <input type="submit" value="Submit">
+</form>
