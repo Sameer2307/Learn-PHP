d42de53b1501342d59483286738bb6af888b14cd
Teja Sudeep <44145755+codeteja@users.noreply.github.com>
2018-10-15 03:01:07 +0530

    Update upload_file.php

--- upload_file.php
+++ upload_file.php
@@ -1,19 +1,34 @@
 <form enctype="multipart/form-data" action="upload.php" method="POST">
     <p>Upload your file</p>
-    <input type="file" name="uploaded_file"></input><br />
+    <input type="file" name="ufile"></input><br />
     <input type="submit" value="Upload"></input>
 </form>
 
-<?PHP
-  if(!empty($_FILES['uploaded_file']))
-  {
-    $path = "uploads/";
-    $path = $path . basename( $_FILES['uploaded_file']['name']);
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
-      " has been uploaded";
-    } else{
-        echo "There was an error uploading the file, please try again!";
-    }
-  }
+<?php
+//set where you want to store files
+//in this example we keep file in folder upload 
+//$HTTP_POST_FILES['ufile']['name']; = upload file name
+//for example upload file name cartoon.gif . $path will be upload/cartoon.gif
+
+$path= "upload/".$HTTP_POST_FILES['ufile']['name'];
+if($ufile !=none)
+{
+if(copy($HTTP_POST_FILES['ufile']['tmp_name'], $path))
+{
+echo "Successful<BR/>"; 
+
+//$HTTP_POST_FILES['ufile']['name'] = file name
+//$HTTP_POST_FILES['ufile']['size'] = file size
+//$HTTP_POST_FILES['ufile']['type'] = type of file
+
+echo "File Name :".$HTTP_POST_FILES['ufile']['name']."<BR/>"; 
+echo "File Size :".$HTTP_POST_FILES['ufile']['size']."<BR/>"; 
+echo "File Type :".$HTTP_POST_FILES['ufile']['type']."<BR/>"; 
+echo "<img src=\"$path\" width=\"150\" height=\"150\">";
+}
+else
+{
+echo "Error";
+}
+}
 ?>
