729b0f0798ac7ff39d40858f5d73ab3884ae883b
Teja Sudeep <44145755+codeteja@users.noreply.github.com>
2018-10-15 02:22:11 +0530

    cookie.php

---
+++ cookie.php
@@ -0,0 +1,8 @@
+<?php
+setcookie(cookie_name, cookie_value, [expiry_time], [cookie_path], [domain], [secure], [httponly]);
+?>
+
+Usage:
+<?php
+setcookie("user_name", "Guru99", time() - 360,'/');
+?>
