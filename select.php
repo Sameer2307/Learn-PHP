ec115654cfb8635e65ba9fab2dd195c2a22a8c61
Teja Sudeep <44145755+codeteja@users.noreply.github.com>
2018-10-15 03:03:32 +0530

    select.php

---
+++ select.php
@@ -0,0 +1,8 @@
+/ Select all columns from all rows.
+"SELECT * FROM table_name"; 
+or
+// Select some column from all rows.
+"SELECT column_name1, column_name2 FROM table_name";
+or
+// Select all coulumns from one row. 
+"SELECT * FROM table_name WHERE column_name=' value in column '";
