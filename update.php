5886e33ecdc6c63a1808f336ba0b18b1c447d865
Teja Sudeep <44145755+codeteja@users.noreply.github.com>
2018-10-15 03:05:34 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,3 +1,5 @@
+<?php
+
 $sql="UPDATE $tbl_name SET first_name='$first_name', last_name='$last_name', email='$email' WHERE id='$id'";
 $result=mysql_query($sql);
 if($result){
@@ -5,3 +7,5 @@ if($result){
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
+
+?>
