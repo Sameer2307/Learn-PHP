14232c450cd471ee0f3528d3b22fd415f7976d78
Teja Sudeep <44145755+codeteja@users.noreply.github.com>
2018-10-15 03:33:41 +0530

    get_method.php

---
+++ get_method.php
@@ -0,0 +1,9 @@
+
+<?php
+   echo "$_GET['name']";
+?> 
+    
+<form action = "<?php $_PHP_SELF ?>" method = "GET"> 
+   Name: <input type = "text" name = "name" />
+   <input type = "submit" /> 
+</form>
